5b7997a22d060b80e1cadcdf3cf6ca78c0df3e21
tomorinn <sasoripegasasu@gmail.com>
2020-02-06 16:07:40 +0900

    バッククオーテーション問題解決

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rbenv_roles, :all
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
